7e5e565baff1f33be67da149ce32f2f692fd3ac8
Kevin Staiger <kstag07@yahoo.com>
2014-05-23 12:45:40 -0700

    submitting oath and comments with all tests passing

--- test/features/auth/sign_in_test.rb
+++ test/features/auth/sign_in_test.rb
@@ -14,7 +14,7 @@ feature "As a user I want to be able to sign in if I have an existing account" d
 
    scenario "sign in with twitter works" do
    visit root_path
-   click_on "Sign In"
+    first(:link, "Sign in").click
    OmniAuth.config.test_mode = true
    Capybara.current_session.driver.request.env['devise.mapping'] = Devise.mappings[:user]
    Capybara.current_session.driver.request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
@@ -23,7 +23,7 @@ feature "As a user I want to be able to sign in if I have an existing account" d
                             uid: '12345',
                             info: { nickname: 'test_twitter_user'},
                             })
-   click_on "Sign in with Twitter"
+   first(:link,"Sign in with Twitter").click
    save_and_open_page
    page.must_have_content "test_twitter_user, you are signed in!"
   end
